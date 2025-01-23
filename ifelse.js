5fa7be09cde759d56a8310d59569148cb27e5e68
Dinesh Borude <borudedinesh2@gmail.com>
2025-01-23 23:50:03 +0530

    elseif

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+function age(age) {
+    // var age = prompt("Enter your age");
+    if (age < 18) {
+        console.log("You are not eligible to vote");    
+    }
+    else {
+        console.log("You are eligible to votee");
+    }
+}
+
+age(6);
